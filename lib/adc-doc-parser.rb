d5d777b56f99f9141eb3f200741e6e92c7e36e67
Daniel Choi <dhchoi@gmail.com>
2011-05-18 22:45:21 -0400

    working on function parsing

--- lib/adc-doc-parser.rb
+++ lib/adc-doc-parser.rb
@@ -15,18 +15,11 @@ doc = Nokogiri::HTML.parse html
 r = {} # result
 
 r[:cocoa_class] = doc.at('a')[:title]
-
 r[:superclasses] = (x = tr_with_text(doc, 'Inherits from')) && x.search('td/div/a').map(&:inner_text)
-
 r[:protocols] = (x = tr_with_text(doc, 'Conforms to')) && x.search('td/div/span/a').map(&:inner_text)
-
 r[:framework] = (x = tr_with_text(doc, 'Framework')) && x.at('td/div').inner_text.strip
-
 r[:availability] = (x = tr_with_text(doc, 'Availability')) && x.at('td/div/div/text()').text.strip
-
-x = tr_with_text(doc, 'Companion guides')
-companion_guides = x && x.search("td/div/span/a").map {|a| a.inner_text.strip}
-r[:companion_guides] = companion_guides
+r[:companion_guides] = (x = tr_with_text(doc, 'Companion guides')) && x.search("td/div/span/a").map {|a| a.inner_text.strip}
 r[:related_sample_code] = (x = tr_with_text(doc, 'Related sample code')) && x.search('td/div/div/span').map {|a| a.inner_text.strip}
 
 div = doc.at('//div[@id="Overview_section"]') 
@@ -36,16 +29,15 @@ end
 
 r[:overview] = fragment_text(div).strip#.strip_leading_whitespace
 
-div = doc.at("#Tasks_section")
+
+
 tasks = []
-if div
-  headers = div.search("h3")
-  tasks = headers.map do |h|
-    title = h.inner_text
-    ul = h.xpath("following-sibling::ul")[0]
-    mytasks = ul.search("li").map {|a| ascii(a.at("a").inner_text)}
-    {title: title, tasks: mytasks}
-  end
+
+tasks = doc.xpath("//h3[@class='tasks']").map do |h|
+  title = h.inner_text
+  ul = h.xpath("following-sibling::ul")[0]
+  mytasks = ul.search("li").map {|a| ascii(a.at("a").inner_text)}
+  {title: title, tasks: mytasks}
 end
 
 taskmap = {}
@@ -59,10 +51,16 @@ tasks.each do |taskgroup|
   end
 end
 
-class_methods = doc.xpath("//div[@class='api classMethod']")
-names = class_methods.map {|div| div.at("h3").inner_text}
 method_divs = doc.xpath("//div[@class='api classMethod']") + doc.xpath("//div[@class='api instanceMethod']") + doc.xpath("//div[@class='api propertyObjC']")
-
 r[:methods] = method_divs.map {|n| parse_method(n, taskmap)}
+
+def parse_function(n, task_map)
+  { name: n.inner_text,
+    declaration: n.xpath("following-sibling::pre")[0].inner_text.strip
+  }
+end
+
+function_divs = doc.xpath("//h3[@class='tight jump function']") 
+r[:functions] = function_divs.map {|n| parse_function(n, taskmap)}
 
 puts r.to_yaml
