b09733ed4c291e4c793917186e4468c70fe62c6d
Daniel Choi <dhchoi@gmail.com>
2011-05-18 21:34:37 -0400

    method parser in separate file

---
+++ lib/method_parser.rb
@@ -0,0 +1,53 @@
+module MethodParser
+  def lynx(s)
+    script = "lynx -stdin -dump -nonumbers"
+    IO.popen(script, "r+") do |pipe|
+      pipe.puts s
+      pipe.close_write
+      pipe.read
+    end.gsub(/^file:\/\/.*$/, '').strip_leading_whitespace
+  end
+
+  def parse_method(n, taskmap)
+    type = n[:class].split(' ')[-1] 
+    typeSymbol = case type
+                 when /instance/
+                   '- '
+                 when /class/
+                   '+ '
+                 else
+                   ''
+                 end
+                   
+    methodname = typeSymbol + n.at('h3[@class^=jump]').inner_text
+
+    declaration = n.at("div[@class='declaration']").inner_text.strip
+
+    parameters = if (x = n.at("div[@class='api parameters']"))
+      x.search("dl/dt").map do |dt|
+        name = dt.inner_text
+        definition = dt.xpath("following-sibling::dd").first#.inner_text
+        #definition = definition.gsub(/\n */, "\n").gsub(/\n{3,}/, "\n\n")
+        definition = lynx(definition)
+        {name: name, definition: definition.strip}
+      end
+    else
+      nil
+    end
+    abstract = (x = n.at("p[@class=abstract]")) && x.inner_text
+    availability = (x = n.at("div[@class='api availability']/ul/li")) && x.inner_text
+    seealso = if (x = n.at("div[@class$=seeAlso]"))
+      x.search("li").map(&:inner_text).map {|z| ascii(z).strip}
+    end
+
+    {name: methodname,
+    type: type,
+     declaration: declaration,
+     parameters: parameters,
+     abstract: abstract,
+     taskgroup: taskmap[methodname],
+     availability: availability,
+     seealso: seealso
+    }
+  end
+end
