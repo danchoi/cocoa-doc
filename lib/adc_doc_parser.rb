e731602862ad01a2af39dbb18dab2279003f3518
Daniel Choi <dhchoi@gmail.com>
2011-05-22 16:10:35 -0400

    notes;rescue

--- lib/adc_doc_parser.rb
+++ lib/adc_doc_parser.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'iconv'
+require 'sqlite3'
 require 'db'
 # Builds the database of Cocoa methods, functions, etc.
 
@@ -12,9 +13,13 @@ html = ARGV.first ? File.read(ARGV.first) : STDIN.read
 html = Iconv.conv("US-ASCII//translit//ignore", "UTF-8", html)
 doc = Nokogiri::HTML.parse html
 
-case doc.at('title').inner_text
-when /Class|Protocol/
-  ClassParser.new(doc).parse
-else
-  OthersParser.new(doc).parse
+begin
+  case doc.at('title').inner_text
+  when /Class|Protocol/
+    ClassParser.new(doc).parse
+  else
+    OthersParser.new(doc).parse
+  end
+rescue Sequel::DatabaseError, SQLite3::ConstraintException
+  puts $!
 end
