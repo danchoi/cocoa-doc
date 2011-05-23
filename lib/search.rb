31e3963060280e1bebea0dd185a3fd418a5c9a1b
Daniel Choi <dhchoi@gmail.com>
2011-05-23 11:35:33 -0400

    search tool

--- lib/search.rb
+++ lib/search.rb
@@ -1,6 +1,15 @@
 require 'sequel'
 db = Sequel.connect 'sqlite://cocoa.db'
 
+query = "%#{ARGV.first}%"
+db['select name, "class" as type from classes_and_protocols where name like ? UNION
+select name, "method" as type from methods where name like ?  UNION
+select name, "other" as type from others where name like ? 
+', query, query,query].each do |x|
+  puts x.inspect
+end
+exit
+
 #db[:classes_and_protocols].each {|x| puts x[:name] }
 db[:methods].each {|x| puts "%.20s %.30s %s" % [x[:class_or_protocol], x[:name], x[:file]] }
 exit
